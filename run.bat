7a3ebc21cdfb9848999346d862252db6135f544a
nicholas-fink-phl <16352920+nicholas-fink-phl@users.noreply.github.com>
2019-06-06 10:34:07 -0400

    Delete run.bat

--- run.bat
+++
@@ -1,3 +0,0 @@
-call "C:\Program Files\ArcGIS\Pro\bin\Python\envs\arcgispro-py3\python.exe" C:\Users\nicholas.fink\PycharmProjects\AGO_inventory\items.py
-call "C:\Program Files\ArcGIS\Pro\bin\Python\envs\arcgispro-py3\python.exe" C:\Users\nicholas.fink\PycharmProjects\AGO_inventory\users.py
-call "C:\Program Files\ArcGIS\Pro\bin\Python\envs\arcgispro-py3\python.exe" C:\Users\nicholas.fink\PycharmProjects\AGO_inventory\service_usage.py
